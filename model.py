a0e53403e5daf0ce6ca53ef625f290a387a27c88
Atul Singh <atulsingh0401@gmail.com>
2020-07-01 21:59:18 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,28 +1,51 @@
 from tensorflow.keras import models
-from tensorflow.keras.layers import Conv2D, MaxPool2D, Flatten, Dense, BatchNormalization, Dropout
+from tensorflow.keras.layers import ZeroPadding2D, MaxPooling2D, Conv2D, Flatten, Dense, Dropout
 from tensorflow.keras import regularizers
 
 def get_model():
     model = models.Sequential()
-    model.add(Conv2D(16, kernel_size=3,
-                     activation='relu',
-                     kernel_regularizer=regularizers.l2(0.),
-                     activity_regularizer=regularizers.l2(0.),
-                     input_shape=(224, 224, 3)))
-    model.add(MaxPool2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.05))
-    model.add(Conv2D(32, kernel_size=3, activation='relu',
-                     kernel_regularizer=regularizers.l2(0.),
-                     activity_regularizer=regularizers.l2(0.)))
-    model.add(MaxPool2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
-    model.add(Dropout(0.05))
+    model.add(ZeroPadding2D((1,1),input_shape=(224,224, 3)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(256, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(ZeroPadding2D((1,1)))
+    model.add(Conv2D(512, (3, 3), activation='relu'))
+    model.add(MaxPooling2D((2,2), strides=(2,2)))
+    
+    model.add(Conv2D(4096, (7, 7), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(4096, (1, 1), activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Conv2D(2622, (1, 1)))
     model.add(Flatten())
-    model.add(Dense(256, activation='relu', # 3000
-                    kernel_regularizer=regularizers.l2(0.),
-                    activity_regularizer=regularizers.l2(0.)))
-    model.add(Dropout(0.05))
     model.add(Dense(3, activation='softmax'))
     return model
 

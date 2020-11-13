bb6a17d6a3f770d348782b68dc2df265e34eaeff
Atul Singh <atulsingh0401@gmail.com>
2020-11-13 14:12:26 +0530

    Update main.py
    Updated for latest change in face_recognition.py file

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from face_recognition import FaceRecognition
 
 
 if __name__ == '__main__':
-    model_name = "face_recognition.h5"
+    model_path = "model"
     image_path = 'test.jpg'
     face_recognition = FaceRecognition()
-    # face_recognition.training()
-    # face_recognition.save_model(model_name)
-    # model = FaceRecognition.load_saved_model(os.path.join("model", model_name))
-    k = FaceRecognition.model_prediction(image_path, os.path.join("model", model_name),
-                                         os.path.join("model", "face_recognition_class_names.npy"))
+    face_recognition.training()
+    face_recognition.save_model(model_name)
+    model = FaceRecognition.load_saved_model(model_path)
+    k = FaceRecognition.model_prediction(image_path, model_path)
     print(f"detected class is {k}")
